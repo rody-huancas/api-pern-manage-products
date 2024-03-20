0aea89dc83fce361b999c1b00d47fc71b63b3001
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-19 19:37:01 -0500

    Add: validation product

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
-
+import { validationResult } from "express-validator";
 
 export const createProduct = async (req: Request, res: Response) => {
+  let errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
   const product = await Product.create(req.body);
   res.json({ data: product })
 };
--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,32 @@
 import { Router } from "express";
 import { createProduct } from "./handlers/product";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json('Desde get');
+router.get("/", (req, res) => {
+  res.json("Desde get");
 });
 
-router.post('/', createProduct);
+router.post(
+  "/",
+  // validaciones
+  body("name")
+    .notEmpty().withMessage("El nombre del producto no puede ir vacío"),
+  body("price")
+    .isNumeric().withMessage("Valor no válido")
+    .notEmpty().withMessage("El precio del producto no puede ir vacío")
+    .custom((value) => value > 0).withMessage("El precio no válido"),
+    
+  createProduct
+);
 
-router.put('/', (req, res) => {
-    res.json('Desde put');
+router.put("/", (req, res) => {
+  res.json("Desde put");
 });
 
-router.delete('/', (req, res) => {
-    res.json('Desde delete');
+router.delete("/", (req, res) => {
+  res.json("Desde delete");
 });
 
 export default router;
